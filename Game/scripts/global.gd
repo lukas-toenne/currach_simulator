6cc19a234e360b404f09d7f37a48eba11986e9b5
Lukas Tönne <lutnne@microsoft.com>
2021-10-08 16:37:34 +0100

    Add simple fps printer.

--- Game/scripts/global.gd
+++ Game/scripts/global.gd
@@ -4,11 +4,19 @@ onready var menu = get_node("/root/Menu")
 
 var level = null
 
+const TIMER_LIMIT = 2.0
+var timer = 0.0
+
 func _process(_delta):
 	if Input.is_action_just_pressed("exit"):
 		_on_exit_pressed()
 	if Input.is_action_just_pressed("fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+
+	timer += _delta
+	if timer > TIMER_LIMIT:
+		print("fps: " + str(Engine.get_frames_per_second()))
+		timer = 0.0
 
 func _on_exit_pressed():
 	if is_instance_valid(level):
